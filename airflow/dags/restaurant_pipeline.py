049e4d788edfe11d35c8685ed35ea4146609eb5e
Le Truong Vu <truongvude@outlook.com>
2026-03-12 04:03:03 +0700

    feat: add gold tasks to airflow dag

--- airflow/dags/restaurant_pipeline.py
+++ airflow/dags/restaurant_pipeline.py
@@ -9,13 +9,14 @@ SPARK_JOB_FOLDER = os.path.join(AIRFLOW_HOME, "spark-job")
 PY_FILE_LOCATION = os.path.join(SPARK_JOB_FOLDER, "lib.zip")
 KAFKA_BOOTSTRAP_SERVER = "kafka:9092"
 
-local_tz = pendulum.timezone("Asia/Ho_Chi_Minh")
+local_tz = pendulum.timezone("Asia/Bangkok")
 
 dag = DAG(
     dag_id="test_dag1",
-    start_date=pendulum.datetime(2026, 3, 6, tz=local_tz),
+    start_date=pendulum.datetime(2026, 3, 10, tz=local_tz),
     schedule=None,
-    max_active_runs=1
+    max_active_runs=1,
+    catchup=False
 )
 
 
@@ -101,6 +102,62 @@ with dag:
             total_executor_cores=1
         )
 
+    @task_group
+    def gold_dim_group():
+        submit_gold_dim_branches_task = SparkSubmitOperator(
+            task_id="submit_gold_dim_branches_task",
+            application=f"{SPARK_JOB_FOLDER}/gold/dim_branches.py",
+            conn_id="spark_default",
+            py_files=PY_FILE_LOCATION,
+            deploy_mode="client",
+            executor_cores=1,
+            total_executor_cores=1
+        )
+
+        submit_gold_dim_customers_task = SparkSubmitOperator(
+            task_id="submit_gold_dim_customers_task",
+            application=f"{SPARK_JOB_FOLDER}/gold/dim_customers.py",
+            conn_id="spark_default",
+            py_files=PY_FILE_LOCATION,
+            deploy_mode="client",
+            executor_cores=1,
+            total_executor_cores=1
+        )
+
+
+        submit_gold_dim_payments_task = SparkSubmitOperator(
+            task_id="submit_gold_dim_payments_task",
+            application=f"{SPARK_JOB_FOLDER}/gold/dim_payments.py",
+            conn_id="spark_default",
+            py_files=PY_FILE_LOCATION,
+            deploy_mode="client",
+            executor_cores=1,
+            total_executor_cores=1
+        )
+
+
+        submit_gold_dim_products_task = SparkSubmitOperator(
+            task_id="submit_gold_dim_products_task",
+            application=f"{SPARK_JOB_FOLDER}/gold/dim_products.py",
+            conn_id="spark_default",
+            py_files=PY_FILE_LOCATION,
+            deploy_mode="client",
+            executor_cores=1,
+            total_executor_cores=1
+        )
+
+    submit_gold_fact_task = SparkSubmitOperator(
+        task_id="submit_gold_fact_task",
+        application=f"{SPARK_JOB_FOLDER}/gold/fact_order_line.py",
+        conn_id="spark_default",
+        py_files=PY_FILE_LOCATION,
+        application_args=["--ingest_date", "{{ logical_date.strftime('%Y-%m-%d') }}"],
+        deploy_mode="client",
+        executor_cores=1,
+        total_executor_cores=1
+    )
+
     silver_dim = silver_dim_group()
     silver_fact = silver_fact_group()
-    submit_bronze_dim_task >> silver_dim >> silver_fact
+    gold_dim = gold_dim_group()
+    submit_bronze_dim_task >> silver_dim >> silver_fact >> gold_dim >> submit_gold_fact_task
